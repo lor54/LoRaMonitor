ce7e5046d0a0123450ad2f6fed8c4c7dc9384d9b
lor54 <lorenzocontessa.dev@gmail.com>
2023-04-27 23:09:45 +0200

    Trash removed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(!isset($_SESSION["email"])) {
-      header("location:./auth.php");
+      header("location:auth.php");
     }
 
     include "pages/dashboard.php";
