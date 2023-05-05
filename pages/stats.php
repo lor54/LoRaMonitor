e8742b1f5634a48ddb3ba70be0233293033e870e
lor54 <lorenzocontessa.dev@gmail.com>
2023-05-05 16:11:56 +0200

    Stats profile image bug fix

--- pages/stats.php
+++ pages/stats.php
@@ -17,8 +17,7 @@
       }}
 ?>
 
-
-  <body>
+<body>
     <?php include "../include/nav.php"; ?>
     <div class="col-md-6 text-center ">
       <div id="map" style="width: 700px; height: 500px;"></div>
@@ -137,5 +136,6 @@ locations.forEach(element => {
       
         
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   </body>
 </html>
