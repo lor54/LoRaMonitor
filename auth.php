6b7977faf997866f63cbb87c490c6854154c62d6
lor54 <lorenzocontessa.dev@gmail.com>
2023-05-23 13:24:06 +0200

    Auth completed

--- auth.php
+++ auth.php
@@ -1,7 +1,5 @@
 <?php
   $actual_page = "auth";
-
-  require_once("include/header.php");
   session_start();
 
   try {
@@ -136,7 +134,8 @@
   <link rel="stylesheet" href="/styles/auth.css">
   <link rel="stylesheet" href="/styles/toast.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
   <script src="/scripts/auth.js"></script>
   <script src="/scripts/toast.js"></script>
 </head>
@@ -246,6 +245,7 @@
               }
             }
           ?>
+    <script  src="/styles/auth.js"></script>
     <?php include "include/footer.php"; ?>
 </body>
 
